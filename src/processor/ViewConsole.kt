8ec04befff65a89eca252b1bbfa4abcc3eb851b7
Igor <GI_Something@protonmail.com>
2020-04-30 15:26:06 +0300

    feat: Menu improvements/changes/fixes

--- src/processor/ViewConsole.kt
+++ src/processor/ViewConsole.kt
@@ -4,6 +4,7 @@ import java.math.BigDecimal
 
 class ViewConsole : ViewInterface {
     private lateinit var controller: Controller
+    private val regexSplit = Regex("[ ,]+")
 
     override fun initialize(controller: Controller) {
         this.controller = controller
@@ -31,12 +32,18 @@ class ViewConsole : ViewInterface {
 
                 val result = when (option) {
                     "1", "2", "3" -> {
-                        println("Enter matrices:")
-                        val r: Array<Array<BigDecimal>>
+                        val result: Array<Array<BigDecimal>>
                         try {
-                            val a = readMatrixFromConsole()
-                            val b = readMatrixFromConsole()
-                            r = when (option) {
+                            print("Enter size of first matrix: ")
+                            val aSize = readMatrixSizeFromConsole()
+                            println("Enter first matrix:")
+                            val a = readMatrixFromConsole(aSize[0], aSize[1])
+                            print("Enter size of second matrix: ")
+                            val bSize = readMatrixSizeFromConsole()
+                            println("Enter second matrix:")
+                            val b = readMatrixFromConsole(bSize[0], bSize[1])
+
+                            result = when (option) {
                                 "1" -> controller.matrixAddition(a, b)
                                 "2" -> throw NotImplementedError()
                                 "3" -> controller.matrixMultiplication(a, b)
@@ -46,18 +53,22 @@ class ViewConsole : ViewInterface {
                             println("\nError, incompatible matrices\n")
                             continue@mainMenu
                         }
-                        r
+                        result
                     }
                     "4" -> {
+                        print("Enter size of matrix: ")
+                        val size = readMatrixSizeFromConsole()
                         println("Enter matrix:")
-                        val a = readMatrixFromConsole()
+                        val a = readMatrixFromConsole(size[0], size[1])
                         println("Enter scalar:")
                         val scalar = readLine()!!.trim().toBigDecimal()
                         controller.matrixScalarMultiplication(a, scalar)
                     }
                     "5" -> {
+                        print("Enter size of matrix: ")
+                        val size = readMatrixSizeFromConsole()
                         println("Enter matrix:")
-                        val a = readMatrixFromConsole()
+                        val a = readMatrixFromConsole(size[0], size[1])
                         throw NotImplementedError() // temp
                     }
                     "0" -> break@mainMenu
@@ -66,25 +77,42 @@ class ViewConsole : ViewInterface {
                         continue@mainMenu
                     }
                 }
-                println()
+                println("\nThe ${getOperationName(option)} result is:")
                 printMatrix(result)
                 println()
             } catch (e: NotImplementedError) {
                 println("\nOperation not implemented\n")
             } catch (e: NumberFormatException) {
                 println("\nIncorrect input, please try again\n")
+            } catch (e: IncorrectInputException) {
+                println("\nIncorrect input, please try again\n")
             }
         }
     }
 
-    private fun readMatrixFromConsole(): Array<Array<BigDecimal>> {
-        val sizes = readLine()!!.trim().split(' ').map { value -> value.toInt() }
-        if (sizes.size != 2) throw IllegalArgumentException()
-        return Array(sizes[0]) {
-            readLine()!!.trim().split(' ')
-                .map { value -> value.toBigDecimal() }
-                .toTypedArray()
+    private fun getOperationName(option: String): String {
+        return when (option) {
+            "1" -> "addition"
+            "2" -> "subtraction"
+            "3" -> "multiplication"
+            "4" -> "scalar multiplication"
+            "5" -> "transposition"
+            else -> throw IllegalArgumentException("Something went wrong")
         }
+    }
+
+    private fun readMatrixSizeFromConsole(): List<Int> {
+        val mSize = readLine()!!.trim(' ', ',').split(regexSplit).map { value -> value.toInt() }
+        return if (mSize.size != 2 || mSize[0] <= 0 || mSize[1] <= 0) throw IncorrectInputException() else mSize
+    }
+
+    private fun readMatrixFromConsole(rows: Int, cols: Int): Array<Array<BigDecimal>> {
+        if (rows <= 0 || cols <= 0) throw IllegalArgumentException()
+        val matrix = Array(rows) {
+            readLine()!!.trim(' ', ',').split(regexSplit).map { value -> value.toBigDecimal() }.toTypedArray()
+        }
+        if (cols != matrix[0].size || !isMatrixCorrect(matrix)) throw IncorrectInputException()
+        return matrix
     }
 
     private fun printMatrix(matrix: Array<Array<BigDecimal>>) {
@@ -96,4 +124,6 @@ class ViewConsole : ViewInterface {
             }
         }
     }
+
+    private class IncorrectInputException : Exception()
 }
